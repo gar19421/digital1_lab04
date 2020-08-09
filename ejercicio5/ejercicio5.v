683af1983bae550cb5c5fb609c952d679841566c
Brandon Garrido <gar19421@uvg.edu.gt>
2020-08-09 09:33:41 -0600

    add exercise 5

--- ejercicio5/ejercicio5.v
+++ ejercicio5/ejercicio5.v
@@ -9,22 +9,71 @@ module gateLevel1(input wire A, B, C, output wire Y);
   not (notA, A);
   not (notB, B);
   not (notC, C);
-  and (w1, notA, notC);
-  and  (w2, A, notB);
-  and (w3, A, C);
+  and (w1, A, notB , notC);
+  and (w2, A, notB, C);
+  and (w3, A, B, C);
   or  (Y, w1, w2, w3);
 
 endmodule
 
+// ECUACION POS
 
+module gateLevel2(input wire A, B, C, output wire Y);
+
+  wire notA, notC, notB, w1, w2, w3, w4, w5;
+
+  not (notA, A);
+  not (notB, B);
+  not (notC, C);
+  or (w1, A, B , C);
+  or (w2, A, B, notC);
+  or (w3, A, notB, C);
+  or (w4, A, notB, notC);
+  or (w5, notA, notB, C);
+  and (Y, w1, w2, w3, w4, w5);
+
+endmodule
+
+
+// ECUACION MINIMIZADA Y = ( AB' + AC )
+
+module gateLevel3(input wire A, B, C, output wire Y);
+
+  wire notA, notC, notB, w1, w2;
+
+  not (notA, A);
+  not (notB, B);
+  not (notC, C);
+  and (w1, A, notB);
+  and (w2, A, C);
+  or  (Y, w1, w2);
+
+endmodule
 
 
 // BEHAVIORAL MODELING EJERCICIO 5
 
 // ECUACION SOP
 
-module operadores1(input wire A, B, C, D, output wire Y);
+module operadores1(input wire A, B, C, output wire Y);
 
-  assign Y = (~B & ~C & ~D) | (A & ~C) | (A & ~D) | (A & ~B);
+  assign Y = (A & ~B & ~C) | (A & ~B & C) | (A & B & C);
+
+endmodule
+
+// ECUACION POS
+
+module operadores2(input wire A, B, C, output wire Y);
+
+  assign Y = (A | B | C) & (A | B | ~C) & (A | ~B | C) & (A | ~B | ~C) & (~A | ~B | C);
+
+endmodule
+
+
+// ECUACION MINIMIZADA Y = ( AB' + AC )
+
+module operadores3(input wire A, B, C, output wire Y);
+
+  assign Y = (A & ~B) | (A & C);
 
 endmodule
--- ejercicio5/ejercicio5_tb.v
+++ ejercicio5/ejercicio5_tb.v
@@ -1,14 +1,17 @@
 module testbench();
 
-reg
-wire
+reg r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18;
+wire out1, out2, out3, out4, out5, out6;
 
-gateLevel1   G1(r1, r2, r3, out1);
-operadores1 OP1(r15, r16, r17, r18, out5);
-
+gateLevel1 G1(r1, r2, r3, out1);
+gateLevel2 G2(r4, r5, r6, out2);
+gateLevel3 G3(r7, r8, r9, out3);
+operadores1 OP1(r10, r11, r12, out4);
+operadores2 OP2(r13, r14, r15, out5);
+operadores3 OP3(r16, r17, r18, out6);
 
 initial begin
-  $display(" ejercicio 1 - tablas");
+  $display(" ejercicio 5 - sistema de alarmas");
   $display("A B C | Y");
   $display("------|---");
   $monitor("%b %b %b | %b ", r1, r2, r3, out1);
@@ -22,11 +25,91 @@ initial begin
   #1 r3 = 1;
 end
 
+initial begin
+  #10
+  $display("\n");
+  $display("A B C | Y");
+  $display("------|---");
+  $monitor("%b %b %b | %b ", r4, r5, r6, out2);
+  r4 = 0; r5 = 0; r6 = 0;
+  #1 r6 = 1;
+  #1 r5 = 1; r6 = 0;
+  #1 r6 = 1;
+  #1 r4 = 1; r5 = 0; r6 = 0;
+  #1 r6 = 1;
+  #1 r5 = 1; r6 = 0;
+  #1 r6 = 1;
+end
 
+
+initial begin
+  #28
+  $display("\n");
+  $display("A B C | Y");
+  $display("------|---");
+  $monitor("%b %b %b | %b ", r7, r8, r9, out3);
+  r7 = 0; r8 = 0; r9 = 0;
+  #1 r9 = 1;
+  #1 r8 = 1; r9 = 0;
+  #1 r9 = 1;
+  #1 r7 = 1; r8 = 0; r9 = 0;
+  #1 r9 = 1;
+  #1 r8 = 1; r9 = 0;
+  #1 r9 = 1;
+end
+
+initial begin
+  #37
+  $display("\n");
+  $display("A B C | Y");
+  $display("------|---");
+  $monitor("%b %b %b | %b ", r10, r11, r12, out4);
+  r10 = 0; r11 = 0; r12 = 0;
+  #1 r12 = 1;
+  #1 r11 = 1; r12 = 0;
+  #1 r12 = 1;
+  #1 r10 = 1; r11 = 0; r12 = 0;
+  #1 r12 = 1;
+  #1 r11 = 1; r12 = 0;
+  #1 r12 = 1;
+end
+
+
+initial begin
+  #46
+  $display("\n");
+  $display("A B C | Y");
+  $display("------|---");
+  $monitor("%b %b %b | %b ", r13, r14, r15, out5);
+  r13 = 0; r14 = 0; r15 = 0;
+  #1 r15 = 1;
+  #1 r14 = 1; r15 = 0;
+  #1 r15 = 1;
+  #1 r13 = 1; r14 = 0; r15 = 0;
+  #1 r15 = 1;
+  #1 r14 = 1; r15 = 0;
+  #1 r15 = 1;
+end
+
+initial begin
+  #55
+  $display("\n");
+  $display("A B C | Y");
+  $display("------|---");
+  $monitor("%b %b %b | %b ", r16, r17, r18, out6);
+  r16 = 0; r17 = 0; r18 = 0;
+  #1 r18 = 1;
+  #1 r17 = 1; r18 = 0;
+  #1 r18 = 1;
+  #1 r16 = 1; r17 = 0; r18 = 0;
+  #1 r18 = 1;
+  #1 r17 = 1; r18 = 0;
+  #1 r18 = 1;
+end
 
 
 initial
-  # $finish;
+  #64 $finish;
 
 initial begin
   $dumpfile("ejercicio5_tb.vcd");
